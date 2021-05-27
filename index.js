9eda8940a5c676815dc693288c49c3dac3f3f82f
JnxyJInxed <nabilala.work@gmail.com>
2021-05-27 23:23:45 +0700

    add auth route

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 //middleware body parser
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({limit: '500mb', extended: true}));
 
 //middleware
 app.use(express.json());
